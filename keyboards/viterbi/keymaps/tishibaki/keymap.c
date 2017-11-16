22f86cdc3435aee4efe80c32a3321103cdac00fd
ishibaki <iroirotomoki@gmail.com>
2017-11-16 20:21:31 +0900

    Add DVLRAK

--- keyboards/viterbi/keymaps/tishibaki/keymap.c
+++ keyboards/viterbi/keymaps/tishibaki/keymap.c
@@ -9,11 +9,16 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 
-#define _QW 0  // QWERTY Layer
-#define _NM 1  // Numpad Layer
-#define _FN 2  // Function Layer
-#define _MO 3  // Mouse Layer
-#define _AD 10 // Adjust Layer
+#define _QW 0    // QWERTY Layer
+#define _NM 1    // Numpad Layer
+#define _FN 2    // Function Layer
+#define _MO 3    // Mouse Layer
+#define _AD 4    // Adjust Layer
+#define _DV 5    // DVORAK Layer
+#define _DVNM 6  // DVORAK Numpad Layer
+#define _DVFN 7  // DVORAK Function Layer
+#define _DVMO 8  // DVORAK Mouse Layer
+#define _DVAD 9  // DVORAK Adjust Layer
 
 // Fillers to make layering more clear
 #define KC_ KC_TRNS
@@ -40,16 +45,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      TO(_NM), KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET,
      TO(_FN), CTL_T(KC_ESCAPE), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(_MO, KC_SCOLON), KC_QUOTE, KC_ENTER,
      TO(_MO), KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_BSLASH, KC_RSHIFT,
-     TO(_AD), KC_LALT, KC_LGUI, LT(_AD, KC_NO), LT(_MO, KC_NO), LT(_FN, KC_F23), LT(_NM, KC_SPACE), LT(_NM, KC_SPACE), LT(_FN, KC_F24), LT(_MO, KC_NO), LT(_AD, KC_NO), KC_RGUI, KC_KC_RALT, KC_DELETE
+     TO(_AD), KC_LALT, KC_LGUI, LT(_AD, KC_NO), LT(_MO, KC_NO), LT(_FN, KC_F23), LT(_NM, KC_SPACE), LT(_NM, KC_SPACE), LT(_FN, KC_F24), LT(_MO, KC_NO), LT(_AD, KC_NO), KC_RGUI, KC_RALT, KC_DELETE
   ),
 
 /* _NM/Numpad
  * ,------------------------------------------------.      ,------------------------------------------------.
  * |   =  |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |TO(QW)| Tab  |  (   |   )  |  [   |   ]  |      |      |      |   7  |   8  |   9  |   0  |   [  |   ]  |
+ * |TO(QW)| Tab  |  (   |   )  |  [   |   ]  |      |      | Del  |   7  |   8  |   9  |   0  |   [  |   ]  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |TO(FN)|Esc/Ct| NUM/ | NUM* | NUM- | NUM+ |   =  |      |      |   4  |   5  |   6  |   .  |      |Enter |
+ * |TO(FN)|Esc/Ct| NUM/ | NUM* | NUM- | NUM+ |   =  |      | Bksp |   4  |   5  |   6  |   .  |      |Enter |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * |TO(MO)| Shift|   <  |   >  |  {   |   }  |      |      |      |   1  |   2  |   3  |   .  |      |Shift |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
@@ -57,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'      `------------------------------------------------'
  */
   [_NM] = KC_KEYMAP(
-     KC_TRANS, KC_TRANS, KC_F1,       KC_F2,          KC_F3,       KC_F4,       KC_F5,    KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,    KC_F11,   KC_F12,
-     TO(_QW),  KC_TRANS, KC_LPRN,     KC_RPRN,        KC_LBRACKET, KC_RBRACKET, KC_NO,    KC_NO, KC_7,  KC_8,  KC_9,  KC_0,      KC_TRANS, KC_TRANS,
-     TO(_FN),  KC_TRANS, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_KP_PLUS,  KC_EQUAL, KC_NO, KC_4,  KC_5,  KC_6,  KC_KP_DOT, KC_NO,    KC_TRANS,
-     TO(_MO),  KC_TRANS, KC_LABK,     KC_RABK,        KC_LCBR,     KC_RCBR,     KC_NO,    KC_NO, KC_1,  KC_2,  KC_3,  KC_KP_DOT, KC_NO,    KC_TRANS,
-     TO(_AD),  KC_TRANS, KC_TRANS,    KC_NO,          KC_F23,      KC_NO,       KC_NO,    KC_NO, KC_0,  KC_NO, KC_NO, KC_TRANS,  KC_TRANS, KC_TRANS
+     KC_TRANS, KC_TRANS, KC_F1,       KC_F2,          KC_F3,       KC_F4,       KC_F5,    KC_F6,     KC_F7, KC_F8, KC_F9, KC_F10,    KC_F11,   KC_F12,
+     TO(_QW),  KC_TRANS, KC_LPRN,     KC_RPRN,        KC_LBRACKET, KC_RBRACKET, KC_NO,    KC_DELETE, KC_7,  KC_8,  KC_9,  KC_0,      KC_TRANS, KC_TRANS,
+     TO(_FN),  KC_TRANS, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_KP_PLUS,  KC_EQUAL, KC_BSPACE, KC_4,  KC_5,  KC_6,  KC_KP_DOT, KC_NO,    KC_TRANS,
+     TO(_MO),  KC_TRANS, KC_LABK,     KC_RABK,        KC_LCBR,     KC_RCBR,     KC_NO,    KC_NO,     KC_1,  KC_2,  KC_3,  KC_KP_DOT, KC_NO,    KC_TRANS,
+     TO(_AD),  KC_TRANS, KC_TRANS,    KC_NO,          KC_F23,      KC_NO,       KC_NO,    KC_NO,     KC_0,  KC_NO, KC_NO, KC_TRANS,  KC_TRANS, KC_TRANS
   ),
 
 /* _FN/Function
@@ -89,26 +94,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,------------------------------------------------.      ,------------------------------------------------.
  * |   =  |   `  |      |      |      |      |      |      |      |      | W_UP |      |      |      | Bksp |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |TO(NM)| Tab  | SPD_2|      |      |      |      |      |      | LCLK | M_UP | RCLK |      |      |      |
+ * |TO(NM)| Tab  | SPD_2|      |      |      | SPD_2|      |      | LCLK | M_UP | RCLK |      |      |      |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |TO(FN)|Esc/Ct| SPD_1| RCLK | MCLK | LCLK |      |      | W_LT |M_LEFT|M_DOWN|M_RGHT| W_RT |      |Enter |
+ * |TO(FN)|Esc/Ct| SPD_1| RCLK | MCLK | LCLK | SPD_1|      |      |M_LEFT|M_DOWN|M_RGHT|      |      |Enter |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |TO(QW)| Shift| SPD_0|      |      |      |      |      |      |      | W_DN |      |      |      |Shift |
+ * |TO(QW)| Shift| SPD_0|      |      |      | SPD_0|      |      |      | W_DN |      |      |      |Shift |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * |TO(AD)|  Alt | GUI  |      |      |  EN  | Space|      |Space |  JP  |      |      | GUI  | Alt  | Del  |
  * `------------------------------------------------'      `------------------------------------------------'
  */
   [_MO] = KC_KEYMAP(
-     KC_TRANS, KC_TRANS, KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,    KC_NO,            KC_NO,      KC_MS_WH_UP,   KC_NO,       KC_NO,          KC_NO,    KC_BSPACE,
-     TO(_NM),  KC_TRANS, KC_MS_ACCEL2, KC_NO,      KC_NO,      KC_NO,      KC_NO,    KC_NO,            KC_MS_BTN1, KC_MS_UP,      KC_MS_BTN2,  KC_NO,          KC_NO,    KC_NO,
-     TO(_FN),  KC_TRANS, KC_MS_ACCEL1, KC_MS_BTN2, KC_MS_BTN3, KC_MS_BTN1, KC_NO,    KC_KC_MS_WH_LEFT, KC_MS_LEFT, KC_MS_DOWN,    KC_MS_RIGHT, KC_MS_WH_RIGHT, KC_NO,    KC_TRANS,
-     TO(_QW),  KC_TRANS, KC_MS_ACCEL0, KC_NO,      KC_NO,      KC_NO,      KC_NO,    KC_NO,            KC_NO,      KC_MS_WH_DOWN, KC_NO,       KC_NO,          KC_NO,    KC_TRANS,
-     TO(_AD),  KC_TRANS, KC_TRANS,     KC_NO,      KC_NO,      KC_F23,     KC_SPACE, KC_SPACE,         KC_F24,     KC_NO,         KC_NO,       KC_TRANS,       KC_TRANS, KC_TRANS
+     KC_TRANS, KC_TRANS, KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,        KC_NO,    KC_NO,      KC_MS_WH_UP,   KC_NO,       KC_NO,    KC_NO,    KC_BSPACE,
+     TO(_NM),  KC_TRANS, KC_MS_ACCEL2, KC_NO,      KC_NO,      KC_NO,      KC_MS_ACCEL2, KC_NO,    KC_MS_BTN1, KC_MS_UP,      KC_MS_BTN2,  KC_NO,    KC_NO,    KC_NO,
+     TO(_FN),  KC_TRANS, KC_MS_ACCEL1, KC_MS_BTN2, KC_MS_BTN3, KC_MS_BTN1, KC_MS_ACCEL1, KC_NO,    KC_MS_LEFT, KC_MS_DOWN,    KC_MS_RIGHT, KC_NO,    KC_NO,    KC_TRANS,
+     TO(_QW),  KC_TRANS, KC_MS_ACCEL0, KC_NO,      KC_NO,      KC_NO,      KC_MS_ACCEL0, KC_NO,    KC_NO,      KC_MS_WH_DOWN, KC_NO,       KC_NO,    KC_NO,    KC_TRANS,
+     TO(_AD),  KC_TRANS, KC_TRANS,     KC_NO,      KC_NO,      KC_F23,     KC_SPACE,     KC_SPACE, KC_F24,     KC_NO,         KC_NO,       KC_TRANS, KC_TRANS, KC_TRANS
   ),
 
 /* _AD/Adjust
  * ,------------------------------------------------.      ,------------------------------------------------.
- * |   =  |   `  |      |      |      |      |      |      |      |      |      |      |      |      | Bksp |
+ * |   =  |   `  |TO(DV)|      |      |      |      |      |      |      |      |      |      |      | Bksp |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * |TO(NM)| Tab  |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
@@ -120,13 +125,117 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'      `------------------------------------------------'
  */
   [_AD] = KC_KEYMAP(
-    KC_TRANS, KC_KC_TRANS, KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_BSPACE,
+    KC_TRANS, KC_TRANS, TO(_DV),  KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_BSPACE,
     TO(_NM),  KC_TRANS,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO,
     TO(_FN),  KC_TRANS,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_TRANS,
     TO(_MO),  KC_TRANS,    KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_TRANS,
     TO(_QW),  KC_TRANS,    KC_TRANS, KC_NO, KC_NO, KC_F23, KC_SPACE, KC_SPACE, KC_F24, KC_NO, KC_NO, KC_TRANS, KC_TRANS, KC_TRANS
-  )
+  ), 
 
+/* _DV/DVORAK
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |  \   |   `  |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  |   [  | Bksp |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(NM)| Tab  |   '  |   ,  |   .  |   P  |   Y  |      |   F  |   G  |   C  |   R  |   L  |   /  |   =  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(FN)|Esc/Ct|   A  |   O  |   E  |   U  |   I  |      |   D  |   H  |   T  |   N  |   S  |   -  |Enter |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(MO)| Shift|   ;  |   Q  |   J  |   K  |   X  |      |   B  |   M  |   W  |   V  |   Z  |   ]  |Shift |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(AD)|  Alt | GUI  |MO(AD)|MO(MO)|EN/_FN|Sp/_NM|      |Sp/_NM|JP/_FN|MO(MO)|MO(AD)| GUI  | Alt  | Del  |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_DV] = KC_KEYMAP(
+     KC_BSLASH, KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRACKET, KC_BSPACE,
+     TO(_DVNM), KC_TAB, KC_QUOTE, KC_COMMA, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLASH, KC_EQUAL,
+     TO(_DVFN), CTL_T(KC_ESCAPE), KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINUS, KC_ENTER,
+     TO(_DVMO), KC_LSHIFT, KC_SCOLON, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RBRACKET, KC_RSHIFT,
+     TO(_DVAD), KC_LALT, KC_LGUI, LT(_DVAD, KC_NO), LT(_DVMO, KC_NO), LT(_DVFN, KC_F23), LT(_DVNM, KC_SPACE), LT(_DVNM, KC_SPACE), LT(_DVFN, KC_F24), LT(_DVMO, KC_NO), LT(_DVAD, KC_NO), KC_RGUI, KC_RALT, KC_DELETE
+  ), 
+
+/* _DVNM/Numpad
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |   =  |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(DV)| Tab  |  (   |   )  |  [   |   ]  |      |      | Del  |   7  |   8  |   9  |   0  |   [  |   ]  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(FN)|Esc/Ct| NUM/ | NUM* | NUM- | NUM+ |   =  |      | Bksp |   4  |   5  |   6  |   .  |      |Enter |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(MO)| Shift|   <  |   >  |  {   |   }  |      |      |      |   1  |   2  |   3  |   .  |      |Shift |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(AD)|  Alt | GUI  |      |      |  EN  |      |      |      |   0  |      |      | GUI  | Alt  | Del  |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_DVNM] = KC_KEYMAP(
+     KC_TRANS,  KC_TRANS, KC_F1,       KC_F2,          KC_F3,       KC_F4,       KC_F5,    KC_F6,     KC_F7, KC_F8, KC_F9, KC_F10,    KC_F11,   KC_F12,
+     TO(_DV),   KC_TRANS, KC_LPRN,     KC_RPRN,        KC_LBRACKET, KC_RBRACKET, KC_NO,    KC_DELETE, KC_7,  KC_8,  KC_9,  KC_0,      KC_TRANS, KC_TRANS,
+     TO(_DVFN), KC_TRANS, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_KP_PLUS,  KC_EQUAL, KC_BSPACE, KC_4,  KC_5,  KC_6,  KC_KP_DOT, KC_NO,    KC_TRANS,
+     TO(_DVMO), KC_TRANS, KC_LABK,     KC_RABK,        KC_LCBR,     KC_RCBR,     KC_NO,    KC_NO,     KC_1,  KC_2,  KC_3,  KC_KP_DOT, KC_NO,    KC_TRANS,
+     TO(_DVAD), KC_TRANS, KC_TRANS,    KC_NO,          KC_F23,      KC_NO,       KC_NO,    KC_NO,     KC_0,  KC_NO, KC_NO, KC_TRANS,  KC_TRANS, KC_TRANS
+  ),
+
+/* _DVFN/Function
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |   =  |   `  |  F13 |  F14 |  F15 |  F16 |  F17 |      |  F18 |  F19 |  F20 |  F21 | F22  | F23  | F24  |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(NM)| Tab  |  (   |   )  |  [   |   ]  |      |      |      | Home | End  | PgUp | PgDn |      |      |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(DV)|Esc/Ct|      |      |  Del | Bksp |      |      | Left | Down |  Up  | Right| PgDn |      |Enter |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(MO)| Shift|   <  |   >  |  {   |   }  |      |      |      |      |      |      |      |      |Shift |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(AD)|  Alt | GUI  |      |      |      | Space|      |Space |      |      |      | GUI  | Alt  | Del  |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_DVFN] = KC_KEYMAP(
+     KC_TRANS,  KC_TRANS, KC_F13,   KC_F14,   KC_F15,    KC_F16,    KC_F17,   KC_F18,   KC_F19,  KC_F20, KC_F21,   KC_F22,    KC_F23,   KC_F24,
+     TO(_DVNM), KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,  KC_TRANS,  KC_NO,    KC_NO,    KC_HOME, KC_END, KC_PGUP,  KC_PGDOWN, KC_NO,    KC_NO,
+     TO(_DV),   KC_TRANS, KC_NO,    KC_NO,    KC_DELETE, KC_BSPACE, KC_NO,    KC_LEFT,  KC_DOWN, KC_UP,  KC_RIGHT, KC_PGDOWN, KC_NO,    KC_TRANS,
+     TO(_DVMO), KC_TRANS, KC_TRANS, KC_TRANS, KC_TRANS,  KC_TRANS,  KC_NO,    KC_NO,    KC_NO,   KC_NO,  KC_NO,    KC_NO,     KC_NO,    KC_TRANS,
+     TO(_DVAD), KC_TRANS, KC_TRANS, KC_NO,    KC_NO,     KC_NO,     KC_SPACE, KC_SPACE, KC_NO,   KC_NO,  KC_NO,    KC_TRANS,  KC_TRANS, KC_TRANS
+  ),
+
+/* _DVMO/Mouse
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |   =  |   `  |      |      |      |      |      |      |      |      | W_UP |      |      |      | Bksp |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(NM)| Tab  | SPD_2|      |      |      | SPD_2|      |      | LCLK | M_UP | RCLK |      |      |      |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(FN)|Esc/Ct| SPD_1| RCLK | MCLK | LCLK | SPD_1|      |      |M_LEFT|M_DOWN|M_RGHT|      |      |Enter |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(DV)| Shift| SPD_0|      |      |      | SPD_0|      |      |      | W_DN |      |      |      |Shift |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(AD)|  Alt | GUI  |      |      |  EN  | Space|      |Space |  JP  |      |      | GUI  | Alt  | Del  |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_DVMO] = KC_KEYMAP(
+     KC_TRANS,  KC_TRANS, KC_NO,        KC_NO,      KC_NO,      KC_NO,      KC_NO,        KC_NO,    KC_NO,      KC_MS_WH_UP,   KC_NO,       KC_NO,    KC_NO,    KC_BSPACE,
+     TO(_DVNM), KC_TRANS, KC_MS_ACCEL2, KC_NO,      KC_NO,      KC_NO,      KC_MS_ACCEL2, KC_NO,    KC_MS_BTN1, KC_MS_UP,      KC_MS_BTN2,  KC_NO,    KC_NO,    KC_NO,
+     TO(_DVFN), KC_TRANS, KC_MS_ACCEL1, KC_MS_BTN2, KC_MS_BTN3, KC_MS_BTN1, KC_MS_ACCEL1, KC_NO,    KC_MS_LEFT, KC_MS_DOWN,    KC_MS_RIGHT, KC_NO,    KC_NO,    KC_TRANS,
+     TO(_DV),   KC_TRANS, KC_MS_ACCEL0, KC_NO,      KC_NO,      KC_NO,      KC_MS_ACCEL0, KC_NO,    KC_NO,      KC_MS_WH_DOWN, KC_NO,       KC_NO,    KC_NO,    KC_TRANS,
+     TO(_DVAD), KC_TRANS, KC_TRANS,     KC_NO,      KC_NO,      KC_F23,     KC_SPACE,     KC_SPACE, KC_F24,     KC_NO,         KC_NO,       KC_TRANS, KC_TRANS, KC_TRANS
+  ),
+
+/* _DVAD/Adjust
+ * ,------------------------------------------------.      ,------------------------------------------------.
+ * |   =  |   `  |TO(QW)|      |      |      |      |      |      |      |      |      |      |      | Bksp |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(NM)| Tab  |      |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(FN)|Esc/Ct|      |      |      |      |      |      |      |      |      |      |      |      |Enter |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(MO)| Shift|      |      |      |      |      |      |      |      |      |      |      |      |Shift |
+ * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
+ * |TO(DV)|  Alt | GUI  |      |      |  EN  | Space|      |Space |  JP  |      |      | GUI  | Alt  | Del  |
+ * `------------------------------------------------'      `------------------------------------------------'
+ */
+  [_DVAD] = KC_KEYMAP(
+    KC_TRANS,  KC_TRANS, TO(_QW),  KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_BSPACE,
+    TO(_DVNM), KC_TRANS, KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO,
+    TO(_DVFN), KC_TRANS, KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_TRANS,
+    TO(_DVMO), KC_TRANS, KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO,    KC_TRANS,
+    TO(_DV),   KC_TRANS, KC_TRANS, KC_NO, KC_NO, KC_F23, KC_SPACE, KC_SPACE, KC_F24, KC_NO, KC_NO, KC_TRANS, KC_TRANS, KC_TRANS
+  ), 
 };
 
 #ifdef AUDIO_ENABLE
