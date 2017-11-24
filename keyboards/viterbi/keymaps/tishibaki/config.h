f96a994134636b3c12e3483be1db5a325355d18b
ishibaki <iroirotomoki@gmail.com>
2017-11-24 14:59:17 +0900

    Change mouse speed

--- keyboards/viterbi/keymaps/tishibaki/config.h
+++ keyboards/viterbi/keymaps/tishibaki/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MOUSEKEY_DELAY             150
 #define MOUSEKEY_INTERVAL          5
-#define MOUSEKEY_MAX_SPEED         1
+#define MOUSEKEY_MAX_SPEED         2
 #define MOUSEKEY_TIME_TO_MAX       5
 #define MOUSEKEY_WHEEL_MAX_SPEED   1
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 1
